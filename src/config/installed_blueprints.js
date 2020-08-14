e461f180e2337d61150fe795c21c8eb7d1048dbd
Crowdbotics <team@crowdbotics.com>
2020-08-14 01:23:30 +0000

    add package blueprint-react-native-dashboard-2-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Dashboard2290280', human_name: 'Dashboard 2 2', access_route: 'Dashboard2290280'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Dashboard2290280Navigator from '../features/Dashboard2290280/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Dashboard2290280: { screen: Dashboard2290280Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
